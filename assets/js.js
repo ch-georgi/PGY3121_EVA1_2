fb8cae2804d819f3638148d1daefb37a4e3f78c8
Chris Georgi <ch.georgi@duocuc.cl>
2024-05-22 22:23:45 -0400

    modificaciones de htmls para ser llamados por funcion js

---
+++ assets/js.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const modalVerOpciones = document.getElementById("modalVerOpciones");
+  const modalTitle = modalVerOpciones.querySelector(".modal-title");
+  const form = modalVerOpciones.querySelector("form");
+  const radioesData = {
+    "@bandanas": {
+      label: "Selecciona un color",
+      options: [
+        { id: "radio1", label: "Rosa" },
+        { id: "radio2", label: "Roja" },
+        { id: "radio3", label: "Azul" },
+        { id: "radio4", label: "Negra" },
+      ],
+    },
+
+    "@faja": {
+      label: "Selecciona un color",
+      options: [
+        { id: "radio1", label: "Rosa estándar" },
+        { id: "radio2", label: "Verde estándar" },
+        { id: "radio3", label: "Amarillo estándar" },
+      ],
+    },
+    "@collarIsab": {
+      label: "Selecciona una talla",
+      options: [
+        { id: "radio1", label: "XS" },
+        { id: "radio2", label: "S" },
+        { id: "radio3", label: "M" },
+        { id: "radio4", label: "L" },
+        { id: "radio5", label: "XL" },
+      ],
+    },
+    "@corbatin": {
+      label: "Selecciona un color",
+      options: [
+        { id: "radio1", label: "Azul estándar" },
+        { id: "radio2", label: "Negro estándar" },
+      ],
+    },
+    "@tag": {
+      label: "Selecciona un diseño",
+      options: [
+        { id: "radio1", label: "Círculo" },
+        { id: "radio2", label: "Cuadro ondulado" },
+        { id: "radio3", label: "Hueso" },
+        { id: "radio4", label: "Corazón" },
+        { id: "radio5", label: "Estrella" },
+        { id: "radio6", label: "Manzana" },
+        { id: "radio7", label: "Círculo y flor" },
+      ],
+    },
+    // Agrega más datos según sea necesario
+  };
+
+  function populateFormWithData(dataKey) {
+    const { label, options } = radioesData[dataKey];
+    modalTitle.textContent = label;
+    form.innerHTML = ""; // Limpiar el contenido del formulario antes de agregar nuevas opciones
+
+    options.forEach((radio) => {
+      const radioElement = document.createElement("div");
+      radioElement.classList.add("form-check");
+      radioElement.innerHTML = `
+          <input class="form-check-input" type="radio" id="${radio.id}"  name="flexRadioProducto" >
+          <label class="form-check-label" for="${radio.id}">${radio.label}</label>
+        `;
+      form.appendChild(radioElement);
+    });
+  }
+
+  modalVerOpciones.addEventListener("show.bs.modal", function (event) {
+    const button = event.relatedTarget;
+    const dataKey = button.getAttribute("data-bs-producto");
+    populateFormWithData(dataKey);
+  });
+});
