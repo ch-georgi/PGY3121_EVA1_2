edc219de3790e5676ce67a463ae8c86cb2f9c03c
[ioplop] <ishk909@gmail.com>
2024-05-26 15:04:06 -0400

    Implementar .json

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,6 +1,44 @@
 $(document).ready(function() {
   const $contenedor = $('#contenido_container');
 
+  // Carga un producto a la tienda.
+  function cargarProducto(producto, tienda){
+    var $tienda = $('#product-container')
+
+    const sale = `<div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem" > Oferta </div>`
+
+    card = producto.multiple?"/cards/productcardmult.html":"/cards/productcard.html"
+
+    
+    $.get(card, function(data) {
+      d = data.replace("!NAME", producto.name)
+      d = d.replace("!PRICE", producto.price)
+      d = d.replace("!IMG", "." + producto.img)
+      if (producto.sale){
+        d = d.replace("!OLDPRICE", producto.oldprice);
+        d = d.replace("!SALE", sale);
+      }
+      else{
+        d = d.replace("!OLDPRICE", "");
+        d = d.replace("!SALE", "");
+      }
+      if (producto.multiple){
+        d = d.replace("!ID", producto.id)
+      }
+      tienda.append(d);
+    });
+  }
+
+  // Carga todos los productos en la tienda.
+  function cargarTienda(tipo){
+    var $tienda = $('#product-container');
+    products.forEach(function(value, index, array){
+      if(value.type === tipo){
+        cargarProducto(value, $tienda);
+      }
+    });
+  }
+
   function cargarPagina(pagina) {
     let url;
     if (pagina === "index.html") {
@@ -17,8 +55,17 @@ $(document).ready(function() {
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.error(`Error al obtener y analizar ${pagina}:`, textStatus, errorThrown);
-      }
+      },
+      async: false
     });
+
+    if (pagina==="tiendaaccesorios.html"){
+      cargarTienda("other");
+    } else if (pagina==="tiendaalimentos.html") {
+      cargarTienda("food");
+    } else if (pagina==="tiendaropas.html") {
+      cargarTienda("clothes");
+    }
   }
 
   function obtenerPaginaDesdeRuta(ruta) {
